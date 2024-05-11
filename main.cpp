dafbd2bf198f6b54fc8ff22a481602a48a40a4e3
Tom Overall <t.overall@hotmail.com>
2024-05-11 00:06:14 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // Standard input/output
+#include <vector>   // Vectors
+#include <cmath>    // Math functions
+#include <cstdlib>  // Random number generation
+#include <ctime>    // Time functions
